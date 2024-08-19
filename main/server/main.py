0f66929297b7aa39b666321483898184832cc7fa
drew <58788781+drew138@users.noreply.github.com>
2024-08-18 22:02:06 -0500

    feat: adds debug flag

--- main/server/main.py
+++ main/server/main.py
@@ -9,6 +9,8 @@ def main():
 
     port = int(os.getenv("PORT", "8080"))
 
+    debug = os.getenv("DEBUG", "FALSE").upper().startswith("T")
+
     app = Flask(__name__, template_folder="/app/templates")
 
     if route not in handler_config:
@@ -18,7 +20,7 @@ def main():
 
     app.route("/", methods=[method])(handler)
 
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=debug)
 
 
 if __name__ == "__main__":
